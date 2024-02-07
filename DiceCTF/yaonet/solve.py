97d9a70186ee50891d418be4a3ebc2ea5bd98d4f
7Rocky <rockygitt@gmail.com>
2024-02-07 04:02:38 +0100

    DiceCTF 2024 Quals

--- DiceCTF/yaonet/solve.py
+++ DiceCTF/yaonet/solve.py
@@ -14,11 +14,9 @@ dp = 0xdfb3ccae87f774d0a97aec2c494ba96da521dfffbbf9e0e7447fb70000
 P = E(0x7bd81aa93dd37b54e1d39c04f4db67dc65e6b9521107dd11c80e871ef3bd614b, 0x1a17ad5a51b6cc7517618881702a23fe8cc6e4bb06af3a5c5c4dc7b84ccf1108)
 
 table_points = {}
-set_points = set()
 K = dp * G
 
 for dl in range(2 ** 16):
-    set_points.add(K.xy())
     table_points[K.xy()] = dl
     K += G
 
@@ -26,8 +24,7 @@ H = 2 ** (8 * 29) * G
 K = P
 
 for dh in range(2 ** 24):
-    if K.xy() in set_points:
-        dl = table_points[K.xy()]
+    if (dl := table_points.get(K.xy())):
         d = dh * 2 ** (8 * 29) + dp + dl
         assert P == d * G
         with open('id_ecdsa', 'w') as f:
